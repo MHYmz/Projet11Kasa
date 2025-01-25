5dd94e8a2165cf620962e3d0f472cb8b601052d4
Mehmet Y <167089887+MHYmz@users.noreply.github.com>
2025-01-26 00:01:13 +0100

    Work in progress : Logique d'interaction

--- src/components/InfoHabitat/InfoHabitat.jsx
+++ src/components/InfoHabitat/InfoHabitat.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./InfoHabitat.scss";
 import "../../Pages/FlatPage/FlatPage.scss";
 
 
 
 export function InfoHabitat(props) {
+        const [isOpen, setIsOpen] = useState (false);
+
+        const toogleText = () => {
+            setIsOpen((prev) => !prev);
+        };
+
     return (
         <div className="apt-container">
             <div className="apt-info">
-                <p className='apt-legend'>
+                <p className='apt-legend' onClick={toogleText}>
                     <span>{props.title}</span>
-                    <i className="fa-solid fa-chevron-up"></i>
+                    <i className={`fa-solid ${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`} ></i>
                 </p>
-                <p className='apt-text'>{props.text}</p>
+               {isOpen &&  <p className='apt-text'>{props.text}</p>}
             </div>
         </div>
     );
